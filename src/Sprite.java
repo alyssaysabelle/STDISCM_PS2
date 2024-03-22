816a11cf1b3539d609f0983a02cbe90045d0e6e0
Richard <94967357+lcdApple@users.noreply.github.com>
2024-03-22 10:33:39 +0800

    Make the penguin move

--- src/Sprite.java
+++ src/Sprite.java
@@ -8,17 +8,21 @@ import javax.imageio.ImageIO;
 public class Sprite {
     private int x;
     private int y;
-    private BufferedImage image; // Image object to hold the sprite image
+    private BufferedImage[] images; // Array to hold multiple sprite images
+    private int currentImageIndex = 0; // Index of the current image
 
     public Sprite(int x, int y) {
         this.x = x;
         this.y = y;
-        loadImage();
+        loadImages(); // Load multiple images
     }
 
-    private void loadImage() {
+    private void loadImages() {
         try {
-            image = ImageIO.read(new File("src/pingu.png")); // Replace "path/to/your/image.png" with your actual image path
+            images = new BufferedImage[3];
+            images[0] = ImageIO.read(new File("src/pingu.png"));
+            images[1] = ImageIO.read(new File("src/pingu2.png"));
+            images[2] = ImageIO.read(new File("src/pingu.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -27,6 +31,15 @@ public class Sprite {
     public void move(int dx, int dy) {
         x += dx;
         y += dy;
+        if (dx != 0 || dy != 0) { // If moving
+            if (currentImageIndex == 0) {
+                currentImageIndex = 1; // Change to pingu2 image
+            } else {
+                currentImageIndex = 0; // Change to pingu image
+            }
+        } else {
+            currentImageIndex = 2; // Change to pingu image when standing still
+        }
     }
 
     public int getX() {
@@ -41,17 +54,16 @@ public class Sprite {
         int canvasWidth = drawPanel.getWidth();
         int canvasHeight = drawPanel.getHeight();
 
-        double scaleX = (double) canvasWidth / image.getWidth();
-        double scaleY = (double) canvasHeight / image.getHeight();
+        double scaleX = (double) canvasWidth / images[currentImageIndex].getWidth();
+        double scaleY = (double) canvasHeight / images[currentImageIndex].getHeight();
 
         double scale = Math.min(scaleX, scaleY);
 
-        int scaledWidth = (int) (image.getWidth() * scale);
-        int scaledHeight = (int) (image.getHeight() * scale);
+        int scaledWidth = (int) (images[currentImageIndex].getWidth() * scale);
+        int scaledHeight = (int) (images[currentImageIndex].getHeight() * scale);
         int centeredX = (canvasWidth / 2) - (scaledWidth / 2);
         int centeredY = (canvasHeight / 2) - (scaledHeight / 2);
 
-        g.drawImage(image, centeredX, centeredY, scaledWidth, scaledHeight, null);
+        g.drawImage(images[currentImageIndex], centeredX, centeredY, scaledWidth, scaledHeight, null);
     }
 }
-
